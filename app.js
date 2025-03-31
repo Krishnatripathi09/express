11242a722690421e1ff14dd281387bb60e8dcdda
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-03-31 17:13:08 +0530

    feat(auth):Signed the jwt and sent it to user when a User Logs-In

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { adminAuth } = require("./middlewares/admin");
 const { connectDB } = require("./config/database");
 const { User } = require("./models/userSchema");
 const { validateSignUpData } = require("./utils/validation");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const app = express();
 app.use(express.json());
 
@@ -24,19 +26,41 @@ app.post("/user", async (req, res) => {
   validateSignUpData(req, res);
   const { firstName, lastName, gender, email, password } = req.body;
 
+  const passwordHash = await bcrypt.hash(password, 10);
+
   try {
     const user = await User({
       firstName,
       lastName,
       gender,
       email,
-      password,
+      password: passwordHash,
     });
     await user.save();
 
     res.send("User Data Saved SuccessFully" + user);
   } catch (err) {
     console.log(err);
+  }
+});
+
+app.post("/signIn", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404).send("User Not Found Please Enter Valid Email 😕  ");
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  const id = user.id;
+  if (isValidPassword) {
+    const token = await jwt.sign({ id: id }, "MySecretKey");
+    res.cookie("token", token);
+    res.status(200).send("Sign In SuccessFull");
+  } else {
+    res.status(400).send("Invalid Password");
   }
 });
 
